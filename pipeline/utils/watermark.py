048c1abe3cba431b049ccc95bbf2d0ce7d0a524c
harshvardhanr39 <harshvardhanr391@gmail.com>
2026-03-27 15:30:07 +0100

    adding pipeline/utils/

---
+++ pipeline/utils/watermark.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_conn():
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
+
+def get_watermark(source_name: str) -> datetime:
+    """
+    get the last processed timestamp for a given source
+    """
+    with get_conn() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                SELECT last_watermark 
+                FROM pipeline_meta.watermarks 
+                WHERE source_name = %s
+            """, (source_name,))
+            row = cursor.fetchone()
+
+    if row:
+        print(f'[{source_name}] Current watermark: {row[0]}')
+        return row[0]
+    
+    # First ever run, go back to 90 days
+    default = datetime.utcnow() - timedelta(days=90)
+    print(f'[{source_name}] No watermark found, defaulting to: {default}')
+    return default
+
+def update_watermark(source_name: str, new_watermark: datetime):
+    """
+    update the last processed timestamp for a given source
+    """
+    with get_conn() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO pipeline_meta.watermarks 
+                    (source_name, last_watermark)
+                VALUES 
+                    (%s, %s)
+                ON CONFLICT (source_name) DO UPDATE 
+                SET 
+                    last_watermark = EXCLUDED.last_watermark,
+                    last_run_at = NOW(),
+                    updated_at = NOW()
+            """, (source_name, new_watermark))
+        conn.commit()
+    print(f'[{source_name}] Updated watermark to: {new_watermark}')
+ 
+
